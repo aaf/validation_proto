ed360d4f3c89a00404a0db045b418bcc00b15351
aaf <aaf@ardith.org>
2011-03-11 11:11:09 -0700

    fix decimal substitution for lat/long in orange model.

--- app/models/orange.rb
+++ app/models/orange.rb
@@ -1,27 +1,29 @@
 class Orange < ActiveRecord::Base
-if false
-  before_save :format_decimal_point
-end
+
   validates :blueberry, :presence => {:message=>'My custom message'}
   validates :cherry, :numericality => true, :uniqueness => true  
-  validates :latitude, :presence => {:message => "is required"}, :format => { :with => /^(-?(90|([0-8]?\d))((\.|,)\d{1,5})?)?$/, :message => "must be between -90 and 90" }
-  validates :longitude, :presence => {:message => "is required"}, :format => { :with => /^(-?(180|((1[0-7]|\d?)\d))((\.|,)\d{1,5})?)?$/, :message => "must be between -180 and 180" }
+  validates :latitude, 
+    :presence => {:message => "is required"}, 
+    :format => { :with => /^(-?(90|([0-8]?\d))((\.|,)\d{1,5})?)?$/, 
+    :message => "must be between -90.0 and 90.0" 
+  }
+  
+  validates :longitude, 
+    :presence => {:message => "is required"}, 
+    :format => { :with => /^(-?(180|((1[0-7]|\d?)\d))((\.|,)\d{1,5})?)?$/, 
+    :message => "must be between -180.0 and 180.0" 
+  }
 
-if false
   def latitude=(value)
-    write_attribute :latitude, value.gsub!(',', '.')
+    logger.info "value = " + value.to_s   
+    logger.info "value.class = " + value.class.to_s 
+    write_attribute :latitude, value.gsub(',','.')
     latitude
   end
 
   def longitude=(value)
-    write_attribute :longitude, value.gsub!(',', '.')
+    write_attribute :longitude, value.gsub(',','.')
     longitude
   end
 
-  private
-  def format_decimal_point
-    write_attribute :latitude, value.gsub!(',', '.')
-    write_attribute :longitude, value.gsub!(',', '.')
-  end
-end
 end
